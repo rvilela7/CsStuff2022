e9acc19a3da3d12d41c3a6604a4279c8ae9014df
Rui Vilela <ruivil@gmail.com>
2022-05-13 17:20:16 +0100

    Alternative with space

--- ArrayStuff/Program.cs
+++ ArrayStuff/Program.cs
@@ -1,13 +1,11 @@
 ﻿string myString = "Gato Sapato";
 
-List<char> stringList = myString.ToCharArray().ToList();
+List<char> stringList = myString.Replace(" ", "").ToCharArray().ToList();
     
 var result = 
     from a in stringList
     from b in stringList
     where a != b
-    && !Char.IsWhiteSpace(a) // better filter on string
-    && !Char.IsWhiteSpace(b)
     select a.ToString() + b.ToString();
 
 result.ToList().ForEach(m => Console.WriteLine(m));
