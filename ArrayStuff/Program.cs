e22c04e2cb5d7734d804a3cf92a515dc93edcbd5
Rui Vilela <ruivil@gmail.com>
2022-05-13 17:15:31 +0100

    Array permuts

---
+++ ArrayStuff/Program.cs
@@ -0,0 +1,14 @@
+﻿string myString = "Gato Sapato";
+
+List<char> stringList = myString.ToCharArray().ToList();
+    
+var result = 
+    from a in stringList
+    from b in stringList
+    where a != b
+    && !Char.IsWhiteSpace(a) // better filter on string
+    && !Char.IsWhiteSpace(b)
+    select a.ToString() + b.ToString();
+
+result.ToList().ForEach(m => Console.WriteLine(m));
+
