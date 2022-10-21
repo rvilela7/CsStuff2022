77c67429af439a1bce08b93c3932299dfd58ba26
Rui Vilela <ruivil@gmail.com>
2022-10-21 10:48:24 +0100

    Add CountryNames translator

---
+++ JsonCountryNames/Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+string CountryLangNames = "{\n\"België\" : {\n\"NL\" : \"België\",\n\"FR\" : \"Belgique\",\n\"DE\" : \"Belgien\",\n\"EN\" : \"Belgium\",\n\"LU\" : \"Belgien\",\n\"BE\" : \"België\"\n},\n\"Nederland\" : {\n\"NL\" : \"Nederland\",\n\"FR\" : \"Pays-Bas\",\n\"DE\" : \"Niederlande\",\n\"EN\" : \"Netherlands\",\n\"LU\" : \"Nederland\",\n\"BE\" : \"Nederland\"\n},\n\"Luxemburg\" : {\n\"NL\" : \"Luxemburg\",\n\"FR\" : \"Luxembourg\",\n\"DE\" : \"Luxemburg\",\n\"EN\" : \"Luxembourg\",\n\"LU\" : \"Luxemburg\",\n\"BE\" : \"Luxemburg\"\n},\n\"Duitsland\" : {\n\"NL\" : \"Duitsland\",\n\"FR\" : \"Allemagne\",\n\"DE\" : \"Deutschland\",\n\"EN\" : \"Germany\",\n\"LU\" : \"Duitsland\",\n\"BE\" : \"Duitsland\"\n},\n\"Frankrijk\" : {\n\"NL\" : \"Frankrijk\",\n\"FR\" : \"France\",\n\"DE\" : \"Frankreich\",\n\"EN\" : \"France\",\n\"LU\" : \"Frankräich\",\n\"BE\" : \"Frankrijk\"\n}\n}";
+
+var data = (JObject)JsonConvert.DeserializeObject(CountryLangNames);
+
+var country = data.Properties().FirstOrDefault(p => p.Name == "België");
+
+Console.WriteLine(country.Name);
+Console.WriteLine(country);
+Console.WriteLine(country.Value["EN"]);
